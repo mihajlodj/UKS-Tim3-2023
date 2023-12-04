5a5648a043dce841ffb91fb19620804edfa53a76
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2023-12-04 15:39:44 +0100

    Sending email with verification code #10

--- backend/github_clone/auth/serializers.py
+++ backend/github_clone/auth/serializers.py
@@ -1,8 +1,14 @@
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
+from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+import random
+import string
+import threading
 
 from main.models import RegistrationCandidate
 
@@ -38,7 +44,18 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
         registration_candidate = RegistrationCandidate.objects.create(
             user=user,
-            code='123456'
+            code=''.join(random.choice(string.digits) for _ in range(6))
         )
         registration_candidate.save()
+        thr = threading.Thread(target=send_email, args=([registration_candidate]), kwargs={})
+        thr.start()
         return user
+
+
+def send_email(registration_candidate):
+    subject = 'Confirm your registration to Github Clone'
+    html_message = render_to_string('verification_code_email.html', {'code': registration_candidate.code, 'name': registration_candidate.user.first_name})
+    plain_message = strip_tags(html_message)
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = [registration_candidate.user.email,]
+    send_mail(subject, plain_message, email_from, recipient_list, html_message=html_message)
--- backend/github_clone/github_clone/settings.py
+++ backend/github_clone/github_clone/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'github_clone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,14 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'projekatisa365@gmail.com'
+EMAIL_HOST_PASSWORD = 'qiyaqfzueciqvlpq'
 
 
 # Internationalization
