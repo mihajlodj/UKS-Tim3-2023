baa4bc2c0b607d63d8533bd8d5025161b4b3e2ac
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2023-12-09 18:22:32 +0100

    Added more test cases

--- backend/github_clone/tests/test_registration.py
+++ backend/github_clone/tests/test_registration.py
@@ -10,6 +10,7 @@ from auth.serializers import RegistrationSerializer
 
 client = APIClient()
 fake = Faker()
+email = 'fake.email@gmail.com'
 
 @pytest.fixture(autouse=True)
 def disable_send_email(monkeypatch):
@@ -27,7 +28,7 @@ def test_register_candidate_success():
         'password': fake.password,
         'first_name': fake.first_name,
         'last_name': fake.last_name,
-        'email': 'milica.popovic720@gmail.com'
+        'email': email
     }
     response = client.post(url, payload)
     assert response.status_code == status.HTTP_201_CREATED
@@ -43,7 +44,7 @@ def test_register_candidate_empty_username():
         'password': fake.password,
         'first_name': fake.first_name,
         'last_name': fake.last_name,
-        'email': 'milica.popovic720@gmail.com'
+        'email': email
     }
     response = client.post(url, payload)
     assert response.status_code == status.HTTP_400_BAD_REQUEST
@@ -59,7 +60,7 @@ def test_register_candidate_empty_password():
         'password': '',
         'first_name': fake.first_name,
         'last_name': fake.last_name,
-        'email': 'milica.popovic720@gmail.com'
+        'email': email
     }
     response = client.post(url, payload)
     assert response.status_code == status.HTTP_400_BAD_REQUEST
@@ -75,7 +76,7 @@ def test_register_candidate_empty_first_name():
         'password': fake.password,
         'first_name': '',
         'last_name': fake.last_name,
-        'email': 'milica.popovic720@gmail.com'
+        'email': email
     }
     response = client.post(url, payload)
     assert response.status_code == status.HTTP_400_BAD_REQUEST
@@ -91,7 +92,7 @@ def test_register_candidate_empty_last_name():
         'password': fake.password,
         'first_name': fake.first_name,
         'last_name': '',
-        'email': 'milica.popovic720@gmail.com'
+        'email': email
     }
     response = client.post(url, payload)
     assert response.status_code == status.HTTP_400_BAD_REQUEST
@@ -116,10 +117,97 @@ def test_register_candidate_empty_email():
 
 
 @pytest.mark.django_db
+def test_register_candidate_duplicate_username():
+    url = '/auth/register/'
+    username = fake.user_name
+    payload1 = {
+        'username': username,
+        'password': fake.password,
+        'first_name': fake.first_name,
+        'last_name': fake.last_name,
+        'email': email
+    }
+    response = client.post(url, payload1)
+    assert response.status_code == status.HTTP_201_CREATED
+    assert User.objects.count() == 1
+    assert RegistrationCandidate.objects.count() == 1
+    payload2 = {
+        'username': username,
+        'password': fake.password,
+        'first_name': fake.first_name,
+        'last_name': fake.last_name,
+        'email': 'fake.email+1@gmail.com'
+    }
+    response = client.post(url, payload2)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert User.objects.count() == 1
+    assert RegistrationCandidate.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_register_candidate_duplicate_email():
+    url = '/auth/register/'
+    payload1 = {
+        'username': fake.user_name,
+        'password': fake.password,
+        'first_name': fake.first_name,
+        'last_name': fake.last_name,
+        'email': email
+    }
+    response = client.post(url, payload1)
+    assert response.status_code == status.HTTP_201_CREATED
+    assert User.objects.count() == 1
+    assert RegistrationCandidate.objects.count() == 1
+    payload2 = {
+        'username': fake.user_name,
+        'password': fake.password,
+        'first_name': fake.first_name,
+        'last_name': fake.last_name,
+        'email': email
+    }
+    response = client.post(url, payload2)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert User.objects.count() == 1
+    assert RegistrationCandidate.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_register_candidate_weak_password1():
+    url = '/auth/register/'
+    payload = {
+        'username': fake.user_name,
+        'password': '123456',
+        'first_name': fake.first_name,
+        'last_name': fake.last_name,
+        'email': email
+    }
+    response = client.post(url, payload)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert User.objects.count() == 0
+    assert RegistrationCandidate.objects.count() == 0
+
+
+@pytest.mark.django_db
+def test_register_candidate_weak_password2():
+    url = '/auth/register/'
+    payload = {
+        'username': fake.user_name,
+        'password': 'asnfejv',
+        'first_name': fake.first_name,
+        'last_name': fake.last_name,
+        'email': email
+    }
+    response = client.post(url, payload)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert User.objects.count() == 0
+    assert RegistrationCandidate.objects.count() == 0
+
+
+@pytest.mark.django_db
 def test_confirm_registration_success():
     username = 'abcd'
     code = '123456'
-    user = User.objects.create_user(username=username, first_name=fake.first_name, last_name=fake.last_name, email='fake@email.com', password='MyPass123')
+    user = User.objects.create_user(username=username, first_name=fake.first_name, last_name=fake.last_name, email=email, password='MyPass123')
     user.save()
     assert User.objects.count() == 1
     candidate = RegistrationCandidate.objects.create(user=user, code=code)
@@ -141,7 +229,7 @@ def test_confirm_registration_success():
 def test_confirm_registration_wrong_username():
     username = 'abcd'
     code = '123456'
-    user = User.objects.create_user(username=username, first_name=fake.first_name, last_name=fake.last_name, email='fake@email.com', password='MyPass123')
+    user = User.objects.create_user(username=username, first_name=fake.first_name, last_name=fake.last_name, email=email, password='MyPass123')
     user.save()
     assert User.objects.count() == 1
     candidate = RegistrationCandidate.objects.create(user=user, code=code)
@@ -163,7 +251,7 @@ def test_confirm_registration_wrong_username():
 def test_confirm_registration_wrong_code():
     username = 'abcd'
     code = '123456'
-    user = User.objects.create_user(username=username, first_name=fake.first_name, last_name=fake.last_name, email='fake@email.com', password='MyPass123')
+    user = User.objects.create_user(username=username, first_name=fake.first_name, last_name=fake.last_name, email=email, password='MyPass123')
     user.save()
     assert User.objects.count() == 1
     candidate = RegistrationCandidate.objects.create(user=user, code=code)
