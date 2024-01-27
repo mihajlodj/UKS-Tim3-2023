62050bc1c46dab8e686bea5195f4e720654b8f66
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2024-01-27 17:15:08 +0100

    Create repository page design #14

---
+++ front/github_clone_front/src/components/repository/CreateRepo.vue
@@ -0,0 +1,94 @@
+<template>
+    <div>
+        <div class="container w-50 py-5">
+            <h3 class="mb-3">Create a new repository</h3>
+            <h6 class="sub">A repository contains all project files, including the revision history</h6>
+            <hr>
+            <h6><i>Required fields are marked with an asterisk (*)</i></h6>
+            <hr>
+
+            <div class="d-flex flex-column">
+                <label class="bold mb-2">Repository name *</label>
+                <input type="text" class="mb-3 repo-name" />
+            </div>
+
+            <div class="d-flex flex-column">
+                <label class="bold mb-2">Description <span>(optional)</span></label>
+                <input type="text" class="w-100" />
+            </div>
+
+            <hr>
+            <div class="d-flex justify-content-start">
+                <input class="form-check-input mt-3" type="radio" name="access-modifier" id="public-radio" checked>
+                <font-awesome-icon icon="fa-solid fa-book-bookmark" class="ms-4 me-3 mt-2" />
+                <div class="d-flex flex-column">
+                    <span class="bold">Public</span>
+                    <span class="smaller">Anyone on the internet can see this repository. You choose who can commit.</span>
+                </div>
+            </div>
+            <div class="d-flex justify-content-start mt-3">
+                <input class="form-check-input mt-3" type="radio" name="access-modifier" id="private-radio">
+                <font-awesome-icon icon="fa-solid fa-lock" class="ms-4 me-3 mt-2" />
+                <div class="d-flex flex-column">
+                    <span class="bold">Private</span>
+                    <span class="smaller">You choose who can see and commit to this repository.</span>
+                </div>
+            </div>
+
+            <hr>
+            <div class="d-flex justify-content-start">
+                <font-awesome-icon icon="fa-solid fa-circle-info" class="me-2" style="margin-top: 6px; color: #7a8188" />
+                <p class="sub">You are creating a private repository in your personal account.</p>
+            </div>
+
+            <hr>
+            <div class="d-flex justify-content-end">
+                <button class="btn">Create repository</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+button {
+    background-color: #20883d;
+    color: white;
+    font-size: large;
+}
+
+.container {
+    max-width: 900px;
+}
+
+h3 {
+    font-weight: 600;
+}
+
+.sub {
+    color: #656e77;
+}
+
+.bold {
+    font-weight: 600;
+    font-size: large;
+}
+
+.repo-name {
+    width: 50%;
+    min-width: 250px;
+    max-width: 350px;
+}
+
+.smaller {
+    font-size: small;
+    color: #656e77;
+}
+
+.fa-book-bookmark, .fa-lock {
+    height: 25px;
+}
+</style>
--- front/github_clone_front/src/main.js
+++ front/github_clone_front/src/main.js
@@ -4,14 +4,21 @@ import App from './App.vue'
 import DeveloperRegistration from './components/auth/DeveloperRegistration.vue'
 import CodeVerification from './components/auth/CodeVerification.vue'
 import LoginPage from './components/auth/LoginPage.vue'
+import CreateRepo from './components/repository/CreateRepo.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import 'vue3-toastify/dist/index.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'
+import { faBookBookmark } from '@fortawesome/free-solid-svg-icons'
+import { faLock } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faAngleRight);
+library.add(faCircleInfo);
+library.add(faBookBookmark);
+library.add(faLock);
 
 const routes = [
     {
@@ -25,6 +32,10 @@ const routes = [
     {
         path: "/",
         component: LoginPage
+    },
+    {
+        path: "/new",
+        component: CreateRepo
     }
 ]
 
