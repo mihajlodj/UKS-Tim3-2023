b11cb92f6f23aee240cf5edc854663b80a4d669f
SimicAleksa <103002749+SimicAleksa@users.noreply.github.com>
2024-01-28 17:14:46 +0100

    Added repository search bar #23

--- front/github_clone_front/src/components/pages/MainPage.vue
+++ front/github_clone_front/src/components/pages/MainPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="entire-page">
     <nav-bar :user="user" />
     <div class="main-content">
       <div class="left-section">
@@ -29,8 +29,8 @@ export default {
         bio: "Your bio goes here",
       },
       repositories: [
-        { id: 1, name: "repo1" },
-        { id: 2, name: "repo2" },
+        { id: 1, name: "SimicAleksa/NvtKts" },
+        { id: 2, name: "SimicAleksa/pythonProject" },
       ],
     };
   },
@@ -38,8 +38,20 @@ export default {
 </script>
 
 <style scoped>
+.entire-page{
+  min-height: 100vh;
+  margin: 0px;
+  padding: 0;
+  background: #24292e;
+}
+
 .main-content {
   display: flex; 
+  overflow: auto;
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  min-height: 92.5vh;
 }
 
 .left-section {
--- front/github_clone_front/src/components/util/MainPageUtil/RepositoryList.vue
+++ front/github_clone_front/src/components/util/MainPageUtil/RepositoryList.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="repository-list">
-    <h2>Your Repositories</h2>
-    <ul>
-      <li v-for="repo in repositories" :key="repo.id">{{ repo.name }}</li>
+    <label id="profile-name"><img src="../../../.. /../assets/git_profile_picture.png" alt="User Avatar" class="profile-picture-main"> SimicAleksa</label>
+    <br>
+    <div class="repo-searchj-new">
+      <div id="top-repo">
+        <label id="repositories-id">Top repositories</label>
+      </div>
+      <div id="btn-new-repo">
+        <button type="button" class="new-repo-btn"><i class="bi bi-journal-plus"></i> &nbsp;New</button>
+      </div>
+    </div>
+    <div id="search-bar-div">
+        <input type="text" v-model="searchQuery" placeholder="Find a repository..." class="search-bar">
+      </div>
+    <ul class="repo-ul">
+      <li class="repo-li" v-for="repo in filteredRepositories" :key="repo.id">
+        <img src="../../../.. /../assets/git_profile_picture.png" alt="User Avatar" class="profile-picture-main">
+        {{ repo.name }}
+      </li>
     </ul>
   </div>
 </template>
@@ -12,37 +27,115 @@ export default {
   props: {
     repositories: Array,
   },
+  data() {
+    return {
+      searchQuery: '',
+    };
+  },
+  computed: {
+    filteredRepositories() {
+      return this.repositories.filter(repo =>
+        repo.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    },
+  },
 };
 </script>
 
 <style scoped>
+#btn-new-repo{
+  margin-inline-start: 3rem;
+}
+
+#search-bar-div{
+  margin-inline-start:1rem;
+  margin-inline-end:1rem;
+}
+
+.repo-searchj-new{
+  display: flex;
+}
+
+#top-repo{
+  float: left;
+}
+#new-repo-btn{
+  float: left;
+}
+
+.search-bar {
+  margin-bottom: 0.5rem;
+  padding: 0.1rem;
+  font-size: 9px;
+  background: black;
+  border:none;
+  color: white;
+  width: 100%;
+}
+
+.new-repo-btn{
+  background: rgb(6, 146, 55);
+  border: none;
+  margin-inline-start: 1rem;
+  margin-top: 0.9rem;
+  margin-inline-end: 1rem;
+  border-radius: 5px;
+  color: white;
+  float: right;
+  font-size: 0.7rem;
+}
+
+.new-repo-btn:hover{
+  background: rgb(7, 181, 68);
+}
+
+#repositories-id{
+  margin-inline-start: 1rem;
+  margin-bottom: 0.5rem;
+  margin-top: 1rem;
+  font-size: 0.8rem;
+}
+
+.profile-picture-main{
+  max-height: 1rem;
+  max-width: 100%;
+  border-radius: 25px;
+}
+
+
 .repository-list {
   background-color: #24292e;
   border-top: 1px solid #929191;
+  border-right: 1px solid #929191;
   color: white;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 }
 
-h2 {
-  font-size: 24px;
+#profile-name {
+  padding: 1rem;
+  font-size: 0.8rem;
   margin-bottom: 10px;
 }
 
-ul {
+.repo-ul {
   list-style-type: none;
   padding: 0;
-  margin: 0;
+  font-size: 0.7rem;
+  margin-inline-start: 1rem;
 }
 
-li {
-  background-color: #c6c3c357;
-  padding: 10px;
+.repo-li {
+  background-color: none;
+  padding: 2px;
   margin-bottom: 5px;
   border-radius: 4px;
   cursor: pointer;
   transition: background-color 0.3s ease;
 }
 
-li:hover {
+.repo-li:hover {
   background-color: #929191;
 }
 </style>
