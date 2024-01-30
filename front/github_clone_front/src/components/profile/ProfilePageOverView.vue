8333f70ba2b9f9a2a0a1867b86e8df88bb873dd6
SimicAleksa <103002749+SimicAleksa@users.noreply.github.com>
2024-01-30 15:29:56 +0100

    Added popular repositories #17

--- front/github_clone_front/src/components/profile/ProfilePageOverView.vue
+++ front/github_clone_front/src/components/profile/ProfilePageOverView.vue
@@ -1,25 +1,102 @@
 <template>
   <div class="main">
-    ss
+    <div class="left-side">
+        <div id="avatar-id">
+            <a href="#" >
+                <img src="../../assets/git_profile_picture.png" alt="User Avatar" class="profile-picture-main"/>
+            </a>
+        </div>
+        <div>
+            <label id="profile_name">SimicAleksa</label>
+        </div>
+        <div id="edit-button-div">
+            <button type="button" class="edit_btn">Edit profile</button>
+        </div>
+    </div>
+    <div class="right-side">
+        <div>
+            <label style="color:white; padding:1rem;font-size:0.8rem">Popular repositories</label>
+        </div>
+        <repository-container/>
+        <repository-container/>
+        <repository-container/>
+        <repository-container/>
+        <repository-container/>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-name: 'ProfilePageOverView',
+import RepositoryContainer from '../profile/RepositoryContainer.vue';
 
-components: {
-   
-},
+export default {
+  name: 'ProfilePageOverView',
+  components: {
+    RepositoryContainer
+  },
 }
 </script>
 
 <style scoped>
-.main{
-    margin-inline-start: 10rem;
-    margin-inline-end: 10rem;
-    background: red;
-    width: 100%;
+#edit-button-div{
+    display: flex;
+    justify-content: center;
 }
 
+.edit_btn{
+    background: rgb(71, 71, 71);
+    border: 1px solid gray;
+    color: rgb(212, 212, 212);
+    font-size: 0.8rem;
+    width: 100%;
+    border-radius: 0.5rem;
+}
+
+.edit_btn:hover{
+    background: rgb(91, 91, 91);
+}
+
+#profile_name{
+    color: white;
+    padding: 0.5rem;
+}
+
+.links:hover{
+ background: rgba(132, 132, 132, 0.623);
+}
+
+#avatar-id{
+    width: auto;
+    justify-content: center;
+    display: flex;
+}
+
+.profile-picture-main{
+  max-height: 10rem;
+  max-width: 100%;
+  border-radius: 5rem;
+  padding: 0.5rem;
+}
+
+
+.left-side {
+  width: 30%;
+  float: left;
+  height: 95%;
+  background: rgb(79, 78, 78);
+}
+
+.right-side {
+  width: 70%;
+  float: left;
+  height: 95%;
+  background: rgb(60, 60, 60);
+}
+
+.main {
+  margin-inline-start: 10rem;
+  margin-inline-end: 10rem;
+  width: 100%;
+  background: red;
+}
 </style>
---
+++ front/github_clone_front/src/components/profile/RepositoryContainer.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="repository-container">
+    <div style="display: flex;">
+      <a id="project-name" href="#">UKS-Project</a>
+    </div>
+    <div id="repo-state">
+      <label id="state-label">private</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+#repo-state {
+  float: right;
+  display: flex;
+  justify-content: center;
+  border: 1px solid gray;
+  padding-top: 0.5rem;
+  padding-inline-start: 0.3rem;
+  padding-inline-end: 0.3rem;
+  color: rgb(225, 225, 225);
+  background: rgb(145, 145, 145);
+  border-radius: 1rem;
+  margin-left: auto;
+}
+
+#state-label{
+  font-size: 0.7rem;
+}
+
+#project-name {
+  padding: 1rem;
+  font-size: 0.9rem;
+  display: block;
+  padding-top: 0.1rem;
+  padding-bottom: 0.1rem;
+  text-decoration: none;
+  color: rgb(12, 29, 213);
+}
+
+.repository-container {
+  float: left;
+  background: rgb(100, 100, 100);
+  border-radius: 1rem;
+  display: flex;
+  padding: 0.2rem;
+  width: 13rem;
+  height: 3rem;
+  margin: 1rem;
+}
+
+</style>
