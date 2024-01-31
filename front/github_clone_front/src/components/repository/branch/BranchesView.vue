f2f43be3b930b207d4ad3b5b364bc75bdff32ac3
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2024-01-31 11:45:02 +0100

    Displaying all branches frontend #18

--- front/github_clone_front/src/components/repository/branch/BranchesView.vue
+++ front/github_clone_front/src/components/repository/branch/BranchesView.vue
@@ -7,7 +7,6 @@
                 <h3>Branches</h3>
                 <button type="button" class="btn btn-create">New branch</button>
             </div>
-
             <table class="table mt-3">
                 <thead>
                     <tr>
@@ -20,8 +19,22 @@
                 <tbody>
                     <tr v-for="b in branchData" :key="b.name">
                         <td>{{ b.name }}</td>
-                        <td></td>
-                        <td></td>
+                        <td>
+                            <div v-if="b.updatedAvatar !== undefined">
+                                <button type="button" class="btn-avatar me-2">
+                                    <img :src="b.updatedAvatar" alt="avatar" class="updated-avatar" />
+                                </button>
+                                <span class="how-long">{{ howLongAgo(b.updatedTimestamp) }}</span>
+                            </div>
+                        </td>
+                        <td>
+                            <button v-if="b.prStatus !== undefined" type="button" class="btn-pr">
+                                <img v-if="b.prStatus == 'Closed'" alt="pr" src="../../../assets/closed_pr_red.png" class="img-pr mb-1" />
+                                <img v-if="b.prStatus == 'Open'" alt="pr" src="../../../assets/open_pr_green.png" class="img-pr mb-1" />
+                                <img v-if="b.prStatus == 'Merged'" alt="pr" src="../../../assets/merged_pr_purple.png" class="img-pr mb-1" />
+                                <span>#{{ b.prId }}</span>
+                            </button>
+                        </td>
                         <td>
                             <button type="button" class="btn btn-trash" @click="deleteBranch">
                                 <font-awesome-icon icon="fa-regular fa-trash-can" />
@@ -72,20 +85,51 @@ export default {
     methods: {
         deleteBranch() {
 
+        },
+
+        howLongAgo(timestamp) {
+            const currentDate = new Date();
+            const previousDate = new Date(timestamp);
+            const seconds = Math.floor((currentDate - previousDate) / 1000);
+            let interval = Math.floor(seconds / 31536000);
+
+            if (interval >= 1) {
+                return interval + " year" + (interval === 1 ? "" : "s") + " ago";
+            }
+            interval = Math.floor(seconds / 2592000);
+            if (interval >= 1) {
+                return interval + " month" + (interval === 1 ? "" : "s") + " ago";
+            }
+            interval = Math.floor(seconds / 86400);
+            if (interval >= 1) {
+                return interval + " day" + (interval === 1 ? "" : "s") + " ago";
+            }
+            interval = Math.floor(seconds / 3600);
+            if (interval >= 1) {
+                return interval + " hour" + (interval === 1 ? "" : "s") + " ago";
+            }
+            interval = Math.floor(seconds / 60);
+            if (interval >= 1) {
+                return interval + " minute" + (interval === 1 ? "" : "s") + " ago";
+            }
+            return Math.floor(seconds) + " second" + (Math.floor(seconds) === 1 ? "" : "s") + " ago";
         }
-    }
+    },
 }
 </script>
 
 <style scoped>
-
-.th-branch, .th-updated, .th-pull, .th-trash {
+.th-branch,
+.th-updated,
+.th-pull,
+.th-trash {
     background-color: #f7f8fa;
     color: #656e77;
     font-weight: 600;
 }
 
-.btn-create, .btn-create:hover {
+.btn-create,
+.btn-create:hover {
     color: white;
     background-color: #20883d;
     height: 90%;
@@ -101,5 +145,40 @@ export default {
 
 .btn-trash {
     padding: 0;
+}
+
+.updated-avatar {
+    height: 25px;
+    border-radius: 50%;
+}
+
+.btn-avatar {
+    padding: 0%;
+    border: none;
+    background: none;
+}
+
+.how-long {
+    font-size: small;
+}
+
+.img-pr {
+    height: 12px;
+    margin-right: 7px;
+}
+
+.btn-pr {
+    background: none;
+    border: 1px solid #656e77;
+    color: black;
+    padding: 0px 10px;
+    margin: 4px 2px;
+    border-radius: 16px;
+    font-size: small;
+    color: #656e77;
+}
+
+table {
+    border: 1px solid #d6dce3;
 }
 </style>
