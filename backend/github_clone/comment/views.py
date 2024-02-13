ceb3b519062a99553bb94d309778f3ff6c8f6918
Lazar Pavlović <lp.pavlovic.001@gmail.com>
2024-02-13 17:22:57 +0100

    Added get all comments for issue, milestone or pull_request on backend. #38

--- backend/github_clone/comment/views.py
+++ backend/github_clone/comment/views.py
@@ -6,11 +6,108 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied
 
-from main.models import Comment
+from main.models import Comment, Issue, Milestone, PullRequest
 
 from comment.serializers import CommentSerializer
+
 
 class CreateCommentView(generics.CreateAPIView):
     queryset = Comment.objects.all()
     permission_classes = (IsAuthenticated,)
     serializer_class = CommentSerializer
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_comments_for_type(request, type_for, type_id):
+    is_valid_type_for = valid_type_for(type_for)
+    if not is_valid_type_for:
+        raise Http404()
+    if not type_id.isdigit():
+        raise Http404()
+    object_exists = check_if_type_id_object_exists(type_id, type_for)
+    if not object_exists:
+        raise Http404()
+    comments = find_all_comments(type_for, type_id)
+    serialized_comments = serialize_comments(comments)
+    return Response(serialized_comments, status=status.HTTP_200_OK)
+
+
+def serialize_comments(comments):
+    result = []
+    for comment in comments:
+        serialized_comment = {
+            'content': comment.content,
+            'parent': comment.parent,
+            'time': comment.time,
+            'developer_id': comment.caused_by.id
+        }
+        if comment.issue is not None:
+            serialized_comment['issue_id'] = comment.issue.id
+        elif comment.milestone is not None:
+            serialized_comment['milestone_id'] = comment.milestone.id
+        elif comment.pull_request is not None:
+            serialized_comment['pull_request'] = comment.pull_request.id
+        result.append(serialized_comment)
+    return result
+
+
+def valid_type_for(type_for):
+    if type_for == "issue":
+        return True
+    elif type_for == "milestone":
+        return True
+    elif type_for == "pull_request":
+        return True
+    return False
+
+
+def check_if_type_id_object_exists(type_id, type_for):
+    if type_for == "issue":
+        return check_if_issue_exists(type_id)
+    elif type_for == "milestone":
+        return check_if_milestone_exists(type_id)
+    elif type_for == "pull_request":
+        return check_if_pull_request_exists(type_id)
+
+
+def check_if_issue_exists(type_id):
+    if Issue.objects.filter(id=type_id).exists():
+        return True
+    return False
+
+
+def check_if_milestone_exists(type_id):
+    if Milestone.objects.filter(id=type_id).exists():
+        return True
+    return False
+
+
+def check_if_pull_request_exists(type_id):
+    if PullRequest.objects.filter(id=type_id).exists():
+        return True
+    return False
+
+
+def find_all_comments(type_for, type_id):
+    if type_for == "issue":
+        return find_all_comments_from_issue(type_id)
+    elif type_for == "milestone":
+        return find_all_comments_from_milestone(type_id)
+    elif type_for == "pull_request":
+        return find_all_comments_from_pull_request(type_id)
+
+
+def find_all_comments_from_issue(issue_id):
+    issue = Issue.objects.get(id=issue_id)
+    return Comment.objects.filter(issue=issue)
+
+
+def find_all_comments_from_milestone(milestone_id):
+    milestone = Milestone.objects.get(id=milestone_id)
+    return Comment.objects.filter(milestone=milestone)
+
+
+def find_all_comments_from_pull_request(pull_request_id):
+    pull_request = PullRequest.objects.get(id=pull_request_id)
+    return Comment.objects.filter(pull_request=pull_request)
