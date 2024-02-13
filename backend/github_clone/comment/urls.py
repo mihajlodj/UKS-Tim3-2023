50f64a2193140495dc3b4827dc1f55df44ee43cf
Lazar Pavlović <lp.pavlovic.001@gmail.com>
2024-02-13 17:22:57 +0100

    Added get all comments for issue, milestone or pull_request on backend. #38

--- backend/github_clone/comment/urls.py
+++ backend/github_clone/comment/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from comment.views import *
 
 urlpatterns = [
-    path('create/', CreateCommentView.as_view(), name='create_comment')
+    path('create/', CreateCommentView.as_view(), name='create_comment'),
+    path('all/<str:type_for>/<str:type_id>', get_comments_for_type, name='get_comments_for_type')
 ]
