98cfbbd3e815c642e1cfc1e2b6e5c1a73801631d
Lazar Pavlović <lp.pavlovic.001@gmail.com>
2024-02-13 18:52:18 +0100

    Added comment delete functionality on backend. #38

--- backend/github_clone/comment/urls.py
+++ backend/github_clone/comment/urls.py
@@ -4,5 +4,6 @@ from comment.views import *
 
 urlpatterns = [
     path('create/', CreateCommentView.as_view(), name='create_comment'),
-    path('all/<str:type_for>/<str:type_id>', get_comments_for_type, name='get_comments_for_type')
+    path('all/<str:type_for>/<str:type_id>', get_comments_for_type, name='get_comments_for_type'),
+    path('delete/<str:comment_id>/', delete_comment, name='delete_comment')
 ]
