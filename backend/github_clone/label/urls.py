567aa355f9cb39b1173306b7d83fcbcbebaa8cb2
Lazar Pavlović <lp.pavlovic.001@gmail.com>
2024-02-14 19:01:08 +0100

    Added get all Labels on backend. #44

--- backend/github_clone/label/urls.py
+++ backend/github_clone/label/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from label.views import *
 
 urlpatterns = [
-    path('create/', CreateLabelView.as_view(), name='create_label')
+    path('create/', CreateLabelView.as_view(), name='create_label'),
+    path('all/', get_labels, name='get_labels')
 ]
