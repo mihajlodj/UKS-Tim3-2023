65acb4fe94148aa60d8665def93c4034d3da6122
Lazar Pavlović <lp.pavlovic.001@gmail.com>
2024-02-16 12:39:34 +0100

    Added Update functionality on backend for Labels. #44

--- backend/github_clone/label/urls.py
+++ backend/github_clone/label/urls.py
@@ -5,5 +5,6 @@ from label.views import *
 urlpatterns = [
     path('create/<str:repository_name>/', CreateLabelView.as_view(), name='create_label'),
     path('all/<str:repository_name>/', get_labels, name='get_labels'),
-    path('delete/<str:label_id>/', delete_label, name='delete_label')
+    path('delete/<str:label_id>/', delete_label, name='delete_label'),
+    path('update/<str:id>/', UpdateLabelView.as_view(), name='update_label')
 ]
