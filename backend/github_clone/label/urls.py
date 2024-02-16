cbf4266ca722031441346675c5c2f550a7dbf320
Lazar Pavlović <lp.pavlovic.001@gmail.com>
2024-02-16 13:30:19 +0100

    Added endpoint for linking Milestone with Label. #44

--- backend/github_clone/label/urls.py
+++ backend/github_clone/label/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('create/<str:repository_name>/', CreateLabelView.as_view(), name='create_label'),
     path('all/<str:repository_name>/', get_labels, name='get_labels'),
     path('delete/<str:label_id>/', delete_label, name='delete_label'),
-    path('update/<str:id>/', UpdateLabelView.as_view(), name='update_label')
+    path('update/<str:id>/', UpdateLabelView.as_view(), name='update_label'),
+    path('link/milestone/<str:label_id>/<str:milestone_id>/', link_label_to_milestone, name='link_label_to_milestone')
 ]
