5592a5e97bef15c855884e4113d4befad7c0afeb
Lazar Pavlović <lp.pavlovic.001@gmail.com>
2024-02-18 16:58:02 +0100

    Added endpoint for linking Issue with Label. #44

--- backend/github_clone/label/urls.py
+++ backend/github_clone/label/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('all/<str:repository_name>/', get_labels, name='get_labels'),
     path('delete/<str:label_id>/', delete_label, name='delete_label'),
     path('update/<str:id>/', UpdateLabelView.as_view(), name='update_label'),
-    path('link/milestone/<str:label_id>/<str:milestone_id>/', link_label_to_milestone, name='link_label_to_milestone')
+    path('link/milestone/<str:label_id>/<str:milestone_id>/', link_label_to_milestone, name='link_label_to_milestone'),
+    path('link/issue/<str:label_id>/<str:issue_id>/', link_label_to_issue, name='link_label_to_issue')
 ]
