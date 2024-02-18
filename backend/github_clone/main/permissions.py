87f708c6c075f8d0d3ac8df6c539de5827d7819c
Lazar Pavlović <lp.pavlovic.001@gmail.com>
2024-02-18 12:12:28 +0100

    Added permissions for Label functionalities on backend. #44

--- backend/github_clone/main/permissions.py
+++ backend/github_clone/main/permissions.py
@@ -71,3 +71,18 @@ class CanUpdateMilestone(BasePermission):
         if role != Role.OWNER and role != Role.MAINTAINER:
             return False
         return True
+
+class CanUpdateLabel(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        repository_name = obj.project.name
+        print(repository_name)
+        if not Project.objects.filter(name=repository_name).exists():
+            return False
+        project = Project.objects.get(name=repository_name)
+        logged_user = request.user.username
+        if not WorksOn.objects.filter(project=project, developer__user__username=logged_user).exists():
+            return False
+        role = WorksOn.objects.get(project=project, developer__user__username=logged_user).role
+        if role != Role.OWNER and role != Role.MAINTAINER:
+            return False
+        return True
