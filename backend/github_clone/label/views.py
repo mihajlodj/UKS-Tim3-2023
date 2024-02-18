1ebed29357b9f4ca0cb92798ff97841c8856c63a
Lazar Pavlović <lp.pavlovic.001@gmail.com>
2024-02-18 17:46:48 +0100

    Added endpoint for linking Pull Request with Label. #44

--- backend/github_clone/label/views.py
+++ backend/github_clone/label/views.py
@@ -89,6 +89,26 @@ def link_label_to_issue(request, label_id, issue_id):
     return Response(status=status.HTTP_200_OK)
 
 
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated, permissions.CanEditRepository])
+def link_label_to_pull_request(request, label_id, pull_request_id):
+    if not label_id.isdigit():
+        raise Http404()
+    if not Label.objects.filter(id=label_id).exists():
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    if not pull_request_id.isdigit():
+        raise Http404()
+    if not Issue.objects.filter(id=pull_request_id).exists():
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    label = Label.objects.get(id=label_id)
+    pull_request = PullRequest.objects.get(id=pull_request_id)
+    if label.project != pull_request.project:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    pull_request.labels.add(label)
+    pull_request.save()
+    return Response(status=status.HTTP_200_OK)
+
+
 def serialize_labels(labels):
     result = []
     for label in labels:
