6c68294c257fbd3f6e25dc7ea9739a8dc8dd6dc9
Lazar Pavlović <lp.pavlovic.001@gmail.com>
2024-05-09 10:28:27 +0200

    Added disabling add and remove buttons when it couldn't be done. #44

--- front/github_clone_front/src/components/label/LabelsModal.vue
+++ front/github_clone_front/src/components/label/LabelsModal.vue
@@ -18,7 +18,7 @@
                 </div>
                 <div class="col-md-4 text-end">
                     <!-- Add button -->
-                    <button class="button-color" @click="linkLabel(label.id)">
+                    <button class="button-color" @click="linkLabel(label.id)" :disabled="label.isSelected === true">
                         <svg viewBox="0 0 16 16" class="" aria-hidden="true" width="16" height="16">
                             <path
                                 d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z">
@@ -27,7 +27,7 @@
                     </button>
 
                     <!-- Remove button -->
-                    <button class="button-color" @click="unlinkLabel(label.id)">
+                    <button class="button-color" @click="unlinkLabel(label.id)" :disabled="label.isSelected === false">
                         <svg viewBox="0 0 16 16" class="" aria-hidden="true" width="16" height="16">
                             <path
                                 d="M3.72 3.72a.75.75 0 0 1 1.06 0L8 6.94l3.22-3.22a.749.749 0 0 1 1.275.326.749.749 0 0 1-.215.734L9.06 8l3.22 3.22a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L8 9.06l-3.22 3.22a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042L6.94 8 3.72 4.78a.75.75 0 0 1 0-1.06Z">
