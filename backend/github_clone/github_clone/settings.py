20a4b10f92956acea396de5bb7e32d33be8f5374
SimicAleksa <103002749+SimicAleksa@users.noreply.github.com>
2024-06-13 20:47:11 +0200

    changed date time to europe #142

--- backend/github_clone/github_clone/settings.py
+++ backend/github_clone/github_clone/settings.py
@@ -157,7 +157,7 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
-ACTIVE_ORIGIN = "http://localhost:3001"
+ACTIVE_ORIGIN = "http://localhost"
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost",
@@ -200,7 +200,7 @@ CACHES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Belgrade'
 
 USE_I18N = True
 
